b4e7a87f81c93a7972ec6170282b06e7f76ae3d5
Rembrandt <nanachi1027@163.com>
2023-10-11 23:26:11 +0800

    update es service interfaces functiondefintion

--- springboot-elasticsearch/src/main/java/com/emma/springbootelasticsearch/elastic/service/IndexService.java
+++ springboot-elasticsearch/src/main/java/com/emma/springbootelasticsearch/elastic/service/IndexService.java
@@ -1,4 +1,75 @@
 package com.emma.springbootelasticsearch.elastic.service;
 
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IndexService {
+    /**
+     * create index mapping
+     * @param indexName
+     * @param mapping
+     */
+    void createMapping(String indexName, XContentBuilder mapping);
+
+    /**
+     * index one document
+     * @param indexName
+     * @param indexType
+     * @param doc
+     */
+    void indexDoc(String indexName, String indexType, Map<String, Object> doc);
+
+    /**
+     * use route to index document
+     * @param indexName
+     * @param indexType
+     * @param doc
+     */
+    void indexDocWithRouting(String indexName, String indexType, String route, Map<String, Object> doc);
+
+    /**
+     * index a series of docs
+     * @param indexName
+     * @param indexType
+     * @param docs
+     */
+    void indexDocs(String indexName, String indexType, List<Map<String, Object>> docs);
+
+    /**
+     *  index support routing a series of Docs
+     *
+     * @param indexName
+     * @param indexType
+     * @param route
+     * @param docs
+     */
+    void indexDocsWithRouting(String indexName, String indexType, String route, List<Map<String, Object>> docs);
+
+    /**
+     * delete a piece of doc
+     * @param indexName
+     * @param indexType
+     * @param id
+     * @return
+     */
+    int deleteDoc(String indexName, String indexType, String id);
+
+    /**
+     * modify a piece of doc
+     * @param indexName
+     * @param indexType
+     * @param doc
+     */
+    void updateDoc(String indexName, String indexType, Map<String, Object> doc);
+
+    /**
+     *  check whether an index really exists.
+     * @param indexName
+     * @return
+     */
+    boolean existIndex(String indexName); 
+
+
 }
--- springboot-elasticsearch/src/main/java/com/emma/springbootelasticsearch/elastic/service/SearchService.java
+++ springboot-elasticsearch/src/main/java/com/emma/springbootelasticsearch/elastic/service/SearchService.java
@@ -1,6 +1,8 @@
 package com.emma.springbootelasticsearch.elastic.service;
 
 import com.emma.springbootelasticsearch.elastic.model.ElasticSearchRequest;
+import com.emma.springbootelasticsearch.elastic.model.GeoDistance;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 
 public interface SearchService {
@@ -9,5 +11,70 @@ public interface SearchService {
      * @param request
      * @return
      */
-    SearchResponse query_string(ElasticSearchRequest request);
+    SearchResponse queryString(ElasticSearchRequest request);
+
+    /**
+     * multi fields query && supports scrolling
+     * @param request
+     * @return
+     */
+    SearchResponse scrollQueryString(ElasticSearchRequest request);
+
+    /**
+     * specific searching
+     * @param index
+     * @param field
+     * @param term
+     */
+    SearchResponse termSearch(String index, String field, String term);
+
+    /**
+     * Global querying
+     *
+     * @param index
+     */
+    SearchResponse matchAllSearch(String index);
+
+    /**
+     *  longitude & latitude querying
+     * @param index
+     * @param geo {longtitude & latitude}
+     * @param pageNumber
+     * @Param pageSize
+     */
+    SearchResponse geoDistanceSearch(String index, GeoDistance geo, Integer pageNumber, Integer pageSize);
+
+    /**
+     * searching nested objects
+     *
+     * @param path
+     * @param  index
+     * @param field
+     * @param value
+     * @param pageNumber
+     * @param pageSize
+     */
+    SearchResponse matchNestedObjectSearch(String path, String index, String field, String value, Integer pageNumber, Integer pageSize);
+
+    /**
+     * join query: query parent value via child index
+     * @param childType
+     * @param index
+     * @param field
+     * @param value
+     * @param pageNum
+     * @param pageSize
+     */
+    SearchResponse hasChildSearch(String childType, String index, String field, String value, Integer pageNum, Integer pageSize);
+
+    /**
+     * join query: query child value via parent index
+     * @param parentType
+     * @param index
+     * @param field
+     * @param value
+     * @param pageNum
+     * @param pageSize
+     */
+    SearchResponse hasParentSearch(String parentType, String index, String field, String value, Integer pageNum, Integer pageSize);
 }
